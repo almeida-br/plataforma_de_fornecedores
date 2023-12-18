49d7ecd83d006981c549fea2270bae48f1775e3e
samara <samaracostad9@gmail.com>
2023-12-18 16:31:50 -0300

    Transferência de arquivos PELA TERCEIRA VEZ SÓ ESSE MÊS e substituição de classes Request por DTO v3.1

--- CRUD/src/main/java/com/spring_boot/CRUD/service/ProductService.java
+++ CRUD/src/main/java/com/spring_boot/CRUD/service/ProductService.java
@@ -1,8 +1,6 @@
 package com.spring_boot.CRUD.service;
 
-import com.spring_boot.CRUD.model.product.RequestProduct;
 import org.springframework.beans.factory.annotation.Autowired;
-import com.spring_boot.CRUD.model.product.Product;
 import com.spring_boot.CRUD.repository.ProductRepository;
 
 public class ProductService {
